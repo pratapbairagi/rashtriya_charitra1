1b35711a5410b6398a006e2d53b71fd28ac48e9e
pratapbairagi <pratapbairagi4cgshop@gmail.com>
2021-11-14 15:37:50 +0530

    input user name update to all user done

--- server_side/index.js
+++ server_side/index.js
@@ -4,7 +4,6 @@ const path = require("path")
 // const http = require("http").Server(app) // also working
 const server = require("http").createServer(app)
 const cors = require("cors")
-const device = require("express-device")
 const port = 5000
 
 
@@ -21,7 +20,9 @@ const io = require("socket.io")(server)
 app.use(cors())
 
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname+"/index.html")
+    // res.sendFile(__dirname+"/index.html")
+    res.send("hellow")
+
 })
 
 // here socket.io runs first time
@@ -30,52 +31,60 @@ io.on("connection", (socket)=>{
     console.log("new connection")
 
     // new user enter in html client
-    socket.on("name_register",({name_,id_})=>{
-        console.log(name_)
-        users_html[socket.id]=name_
-        io.emit("entered_name", {u_name:users_html[socket.id], id:socket.id})
-    })
+    // socket.on("name_register",({name_,id_})=>{
+    //     console.log(name_)
+    //     users_html[socket.id]=name_
+    //     io.emit("entered_name", {u_name:users_html[socket.id], id:socket.id})
+    // })
 
-    /// for login page
-    socket.on("get_users_pool", ({deviceId,name,id})=>{
+
+    // for login page
+    socket.on("send_user_input", ({deviceId,name,id})=>{
         users[socket.id]= name
         ids[socket.id]=id
         devices[socket.id]=deviceId
-        io.emit("sent_users_pool", 
+        socket.emit("receive_user_input", 
         {
             user:users[socket.id],
             id:ids[socket.id],
             device:devices[socket.id]
         })
-   
     })
-    /// for login
+
+    socket.on("send_all_user_pool",({user_name,user_id,device_id})=>{
+        devices[socket.id]=device_id
+        ids[socket.id]=user_id
+        users[socket.id]=user_name
+        console.log("all users--->>",user_name)
+        io.emit("receive_all_users_pool", {user_names:users[socket.id],user_ids:ids[socket.id],device_ids:devices[socket.id]})
+    })
+    // for login
 
     
         
     
 
     // recieved the user name from client side
-    socket.on("visitor", ({visitor_name,id})=>{
-        users[socket.id]=visitor_name
-        console.log(visitor_name)
-        // send the name and id to all clients(client side)
-       io.emit("enter_user",{user:users[socket.id], id:socket.id})
-    })
+    // socket.on("visitor", ({visitor_name,id})=>{
+    //     users[socket.id]=visitor_name
+    //     console.log(visitor_name)
+    //     // send the name and id to all clients(client side)
+    //    io.emit("enter_user",{user:users[socket.id], id:socket.id})
+    // })
         // receiving updated user_names and ids from client side
-    socket.on("send_updated_users", ({user,id})=>{
-        users[socket.id]=user
+    // socket.on("send_updated_users", ({user,id})=>{
+    //     users[socket.id]=user
         
-        // again sending the update user-names and ids to all clients(client side)
-        io.emit("rec_updated_users", {all_users:users[socket.id], id:id})
-    })
+    //     // again sending the update user-names and ids to all clients(client side)
+    //     io.emit("rec_updated_users", {all_users:users[socket.id], id:id})
+    // })
 
     // receiving messages, ids from client side
-    socket.on("message_ready", ({message,id})=>{
-        // sending messages and id s to the all users(client side) 
-        io.emit("message_send",{name:users[socket.id],id:socket.id,messages:message})
-        console.log(message)
-    })
+    // socket.on("message_ready", ({message,id})=>{
+    //     // sending messages and id s to the all users(client side) 
+    //     io.emit("message_send",{name:users[socket.id],id:socket.id,messages:message})
+    //     console.log(message)
+    // })
 
     socket.on("disconnect", ()=>{
         console.log("user left")
@@ -85,6 +94,30 @@ io.on("connection", (socket)=>{
 server.listen(port, ()=>{
     console.log(`listen server http://localhost:${port}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
